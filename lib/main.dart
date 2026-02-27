faebd56344017f6e1761a00712ea5d7909168561
sakai-aigga <sajin_btechedit2023@kusoed.edu.np>
2026-02-27 07:21:30 +0545

    Removed try catch for App run error

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:my_app/core/services/auth_session_storage.dart';
+import 'package:my_app/pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,17 +17,36 @@ void main() async {
   } catch (e) {
     print('Firebase initialization failed: $e');
   }
+
+  Widget initialScreen = const LoginPage();
+
   try {
-    runApp(const MyApp());
+    if (await AuthSessionStorage.isLoggedIn()) {
+      final creds = await AuthSessionStorage.getCredentials();
+      if (creds['email'] != null && creds['password'] != null) {
+        try {
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: creds['email']!,
+            password: creds['password']!,
+          );
+          initialScreen = const HomePage();
+        } catch (e) {
+          print('Auto-login failed: $e');
+          await AuthSessionStorage.clearSession();
+        }
+      }
+    }
   } catch (e) {
-    print('App run failed: $e');
+    print('Error checking login status: $e');
   }
+
+  runApp(MyApp(initialScreen: initialScreen));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget initialScreen;
+  const MyApp({super.key, required this.initialScreen});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     print('MyApp build called');
@@ -36,7 +58,7 @@ class MyApp extends StatelessWidget {
         colorSchemeSeed: Colors.indigoAccent,
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: LoginPage(),
+      home: initialScreen,
     );
   }
 }
